cdc0c29d8dd115f253a8c37fd78f0f126ed33695
Vítor <vitorhugo8899o@gmail.com>
2026-05-15 20:38:30 -0300

    feat: função de deslogar usuário

--- src/servicies/Users.js
+++ src/servicies/Users.js
@@ -57,5 +57,28 @@ async function Login(data) {
 }
 
 
+async function Logout() {
+    try {
+        const response = await api("/api/v1/logout", {
+            method: "POST",
+            credentials: 'include'
+        }
+        )
+        return response
+    } catch (error) {
+        let errorMessage = "Erro de conexão com o servidor. Tente novamente.";
+
+        if (error.response) {
+            const status = error.response.status;
+            const detail = error.response.data?.detail;
+
+            errorMessage = `Erro ao deslogar usuário. Status: ${status}, detalhe: ${detail}`
+        }
+
+        throw new Error(errorMessage);
+    }
+}
+
+
 
 export { CreateUser, Login }
